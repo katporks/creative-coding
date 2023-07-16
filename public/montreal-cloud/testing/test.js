6bd8c79ec9f9ebbf66dfdc754a65583558070553
Kat Olaguer <katherine.olaguer@gmail.com>
2023-07-15 20:28:46 -0700

    added Cube initialization tests

--- public/montreal-cloud/testing/test.js
+++ public/montreal-cloud/testing/test.js
@@ -8,7 +8,7 @@ const Cube = require('../cube');
 describe ('Cloud tests', function() {
     let cloud;
 
-    this.beforeEach(function() {
+    beforeEach(function() {
         cloud = new Cloud();
     })
 
@@ -21,12 +21,25 @@ describe ('Cloud tests', function() {
 describe ('Cube tests', function() {
     let cube;
 
-    this.beforeEach(function() {
-        cube = new Cube();
+    beforeEach(function() {
+        let hasShadeA = true;
+        let hasShadeB = true;
+        let hasShadeC = true;
+        let hasShadeD = true;
+        cube = new Cube(hasShadeA, hasShadeB, hasShadeC, hasShadeD);
     })
 
     it('should be an object', function(done) {
         expect(cube).to.be.a('object');
         done();
     })
+
+    it('should store initial values without mutation', function(done) {
+        expect(cube.hasShadeA).to.be.equal(true);
+        expect(cube.hasShadeB).to.be.equal(true);
+        expect(cube.hasShadeC).to.be.equal(true);
+        expect(cube.hasShadeD).to.be.equal(true);
+        expect(cube.colour).to.be.equal("F2F5F6");
+        done();
+    }) 
 })
