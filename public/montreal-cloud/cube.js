2938ad85d4fbb0c52869efe6bc498a75577b6797
Kat Olaguer <katherine.olaguer@gmail.com>
2023-07-16 00:48:25 -0700

    add package updates

---
+++ public/montreal-cloud/cube.js
@@ -0,0 +1,17 @@
+class Cube {
+    constructor(hasShadeA, hasShadeB, hasShadeC, hasShadeD, colour="F2F5F6") {
+        this.hasShadeA = hasShadeA; 
+        this.hasShadeB = hasShadeB; 
+        this.hasShadeC = hasShadeC;
+        this.hasShadeD = hasShadeD;
+        this.colour = colour; 
+    }
+
+    // TODO: needs validation setters for booleans
+    // ...
+
+    // TODO: needs validation setter for colour
+    // ...
+}
+
+module.exports = Cube;
